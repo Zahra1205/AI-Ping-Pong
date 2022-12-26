cb0b13a9703b82de73ed5426f05527209343c1b3
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2022-12-26 20:50:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,15 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  pose = ml5.poseNet(video, modelLoaded);
 }
-
+function modelLoaded(){
+  console.log("Model is loaded");
+}
 
 function draw(){
 
